d52d80b0b6e6e440a66a3c056262ebb0cc080e89
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-08-07 14:03:15 +0200

    Sec27. Preparacion despliegue en Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [  
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI,{ dbName: process.env.MONGO_DB_NAME }),
     AuthModule,
   ],
   controllers: [AppController],
